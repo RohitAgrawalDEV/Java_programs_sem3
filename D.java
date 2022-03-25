f72bd75fadc827bfdd83d74a126176b49154dd4f
RohitAgrawalDEV <83936637+RohitAgrawalDEV@users.noreply.github.com>
2022-03-25 16:23:40 +0530

    Create D.java

---
+++ D.java
@@ -0,0 +1,18 @@
+package p2;
+
+
+
+public class D {
+
+	public D() {
+		System.out.println("Class D's constructor");
+
+	}
+
+	public void dispD() {
+
+		System.out.println("Display D's method");
+
+	}
+
+}
