c703dd24c5cd9f345716aa6ef35ced31582d2b0f
RohitAgrawalDEV <83936637+RohitAgrawalDEV@users.noreply.github.com>
2022-03-25 16:21:36 +0530

    Create A.java

---
+++ A.java
@@ -0,0 +1,18 @@
+package p1;
+
+
+
+public class A {
+
+	public A() {
+		System.out.println("Class A's constructor");
+
+	}
+
+	public void dispA() {
+
+		System.out.println("Display A's method");
+
+	}
+
+}
